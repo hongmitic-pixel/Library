447da9cb0d09302084632ce25b2f99062baf14e2
hongmitic-pixel <hongmi.tic@gmail.com>
2026-09-18 15:45:52 +0700

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,74 @@ st.write("Dành riêng cho dự án Xây dựng Dân dụng & Hạ tầng Civil 
 # Đường link xuất bản dữ liệu thô dạng CSV từ file Google Sheets của anh
 GOOGLE_SHEET_URL = "https://google.com"
 
-# Hàm nạp dữ liệu an toàn tích hợp kho dự phòng độc lập
+# Danh sách toàn bộ 482 thành phố California chạy trực tiếp trong bộ nhớ
+backup_cities = [
+    "Adelanto", "Agoura Hills", "Alameda", "Albany", "Alhambra", "Aliso Viejo", "Alturas", "Amador City", 
+    "American Canyon", "Anaheim", "Anderson", "Angels Camp", "Antioch", "Apple Valley", "Arcadia", "Arcata", 
+    "Arroyo Grande", "Artesia", "Arvin", "Atascadero", "Atherton", "Atwater", "Auburn", "Avalon", "Avenal", 
+    "Azusa", "Bakersfield", "Baldwin Park", "Banning", "Barstow", "Beaumont", "Bell", "Bell Gardens", 
+    "Bellflower", "Belmont", "Belvedere", "Benicia", "Berkeley", "Beverly Hills", "Big Bear Lake", "Biggs", 
+    "Bishop", "Blue Lake", "Blythe", "Bradbury", "Brawley", "Brea", "Brentwood", "Brisbane", "Buellton", 
+    "Buena Park", "Burbank", "Burlingame", "Calabasas", "Calexico", "California City", "Calimesa", "Calistoga", 
+    "Camarillo", "Campbell", "Canyon Lake", "Capitola", "Carlsbad", "Carmel-by-the-Sea", "Carpinteria", 
+    "Carson", "Cathedral City", "Ceres", "Cerritos", "Chico", "Chino", "Chino Hills", "Chula Vista", 
+    "Citrus Heights", "Claremont", "Clayton", "Clearlake", "Cloverdale", "Clovis", "Coachella", "Coalinga", 
+    "Colfax", "Colma", "Colton", "Colusa", "Commerce", "Compton", "Concord", "Corcoran", "Corning", 
+    "Corona", "Coronado", "Corte Madera", "Costa Mesa", "Cotati", "Covina", "Crescent City", "Cudahy", 
+    "Cupertino", "Cypress", "Daly City", "Dana Point", "Danville", "Davis", "Del Mar", "Del Rey Oaks", 
+    "Delano", "Desert Hot Springs", "Diamond Bar", "Dinuba", "Dixon", "Dorris", "Dos Palos", "Downey", 
+    "Duarte", "Dublin", "Dunsmuir", "East Palo Alto", "Eastvale", "El Cajon", "El Centro", "El Cerrito", 
+    "El Monte", "El Segundo", "Elk Grove", "Emeryville", "Encinitas", "Escalon", "Escondido", "Etna", 
+    "Eureka", "Exeter", "Fairfax", "Fairfield", "Farmersville", "Ferndale", "Fillmore", "Firebaugh", 
+    "Folsom", "Fontana", "Fort Bragg", "Fort Jones", "Fortuna", "Foster City", "Fountain Valley", "Fowler", 
+    "Fremont", "Fresno", "Fullerton", "Galt", "Garden Grove", "Gardena", "Gilroy", "Glendale", "Glendora", 
+    "Goleta", "Gonzalez", "Grand Terrace", "Grass Valley", "Greenfield", "Gridley", "Grover Beach", 
+    "Guadalupe", "Gustine", "Half Moon Bay", "Hanford", "Hawaiian Gardens", "Hawthorne", "Hayward", 
+    "Healdsburg", "Hemet", "Hercules", "Hermosa Beach", "Hesperia", "Hidden Hills", "Highland", "Hillsborough", 
+    "Hollister", "Holtville", "Hughson", "Huntington Beach", "Huntington Park", "Imperial", "Imperial Beach", 
+    "Indian Wells", "Indio", "Industry", "Inglewood", "Ione", "Irvine", "Irwindale", "Isleton", "Jackson", 
+    "Jurupa Valley", "Kerman", "King City", "Kingsburg", "La Cañada Flintridge", "La Habra", "La Habra Heights", 
+    "La Mesa", "La Mirada", "La Palma", "La Puente", "La Quinta", "La Verne", "Lafayette", "Laguna Beach", 
+    "Laguna Hills", "Laguna Niguel", "Laguna Woods", "Lake Elsinore", "Lake Forest", "Lakeport", "Lakewood", 
+    "Lancaster", "Larkspur", "Lathrop", "Lawndale", "Lemon Grove", "Lemoore", "Lincoln", "Lindsay", 
+    "Live Oak", "Livermore", "Livingston", "Lodi", "Loma Linda", "Lomita", "Lompoc", "Long Beach", 
+    "Loomis", "Los Alamitos", "Los Altos", "Los Altos Hills", "Los Angeles", "Los Banos", "Los Gatos", 
+    "Loyalton", "Lynwood", "Madera", "Malibu", "Mammoth Lakes", "Manhattan Beach", "Manteca", "Maricopa", 
+    "Marina", "Martinez", "Marysville", "Maywood", "McFarlin", "Mendota", "Menlo Park", "Merced", 
+    "Mill Valley", "Millbrae", "Milpitas", "Mission Viejo", "Modesto", "Monrovia", "Montague", "Montclair", 
+    "Monte Sereno", "Montebello", "Monterey", "Monterey Park", "Moorpark", "Moraga", "Moreno Valley", 
+    "Morgan Hill", "Morro Bay", "Mount Shasta", "Mountain View", "Murrieta", "Napa", "National City", 
+    "Needles", "Nevada City", "Newark", "Newman", "Newport Beach", "Norco", "Norwalk", "Novato", 
+    "Oakdale", "Oakland", "Oakley", "Oceanside", "Ojai", "Ontario", "Orange", "Orange Cove", "Orland", 
+    "Oroville", "Oxnard", "Pacific Grove", "Pacifica", "Palm Desert", "Palm Springs", "Palmdale", 
+    "Palo Alto", "Palos Verdes Estates", "Paradise", "Paramount", "Parlier", "Pasadena", "Paso Robles", 
+    "Patterson", "Perris", "Petaluma", "Pico Rivera", "Piedmont", "Pinole", "Pismo Beach", "Pittsburg", 
+    "Placentia", "Placerville", "Pleasant Hill", "Pleasanton", "Plymouth", "Point Arena", "Pomona", 
+    "Port Hueneme", "Porterville", "Portola", "Portola Valley", "Poway", "Rancho Cordova", "Rancho Cucamonga", 
+    "Rancho Mirage", "Red Bluff", "Redding", "Redlands", "Redondo Beach", "Redwood City", "Reedley", 
+    "Rialto", "Richmond", "Ridgecrest", "Rio Dell", "Rio Vista", "Ripon", "Riverbank", "Riverside", 
+    "Rocklin", "Rohnert Park", "Rolling Hills", "Rolling Hills Estates", "Rosemead", "Roseville", "Ross", 
+    "Sacramento", "Salinas", "San Anselmo", "San Bernardino", "San Bruno", "San Carlos", "San Clemente", 
+    "San Diego", "San Dimas", "San Fernando", "San Francisco", "San Gabriel", "San Jacinto", "San Joaquin", 
+    "San Jose", "San Juan Bautista", "San Juan Capistrano", "San Leandro", "San Luis Obispo", "San Marcos", 
+    "San Marino", "San Mateo", "San Pablo", "San Rafael", "San Ramon", "Sand City", "Sanger", "Santa Ana", 
+    "Santa Barbara", "Santa Clara", "Santa Clarita", "Santa Cruz", "Santa Fe Springs", "Santa Maria", 
+    "Santa Monica", "Santa Paula", "Santa Rosa", "Santee", "Saratoga", "Sausalito", "Scotts Valley", 
+    "Seal Beach", "Seaside", "Sebastopol", "Selma", "Shafter", "Shasta Lake", "Sierra Madre", "Signal Hill", 
+    "Simi Valley", "Solana Beach", "Soledad", "Solvang", "Sonoma", "Sonora", "South El Monte", "South Gate", 
+    "South Lake Tahoe", "South Pasadena", "South San Francisco", "St. Helena", "Stanton", "Stockton", 
+    "Suisun City", "Sunnyvale", "Susanville", "Sutter Creek", "Taft", "Tehachapi", "Tehama", "Temecula", 
+    "Temple City", "Thousand Oaks", "Tiburon", "Torrance", "Tracy", "Trinidad", "Truckee", "Tulare", 
+    "Tulelake", "Turlock", "Tustin", "Twentynine Palms", "Ukiah", "Union City", "Upland", "Vacaville", 
+    "Vallejo", "Ventura", "Vernon", "Victorville", "Villa Park", "Visalia", "Vista", "Walnut", "Walnut Creek", 
+    "Wasco", "Waterford", "Watsonville", "Weed", "West Covina", "West Hollywood", "West Sacramento", 
+    "Westlake Village", "Westminster", "Westmorland", "Wheatland", "Whittier", "Wildomar", "Williams", 
+    "Willits", "Willows", "Windsor", "Winters", "Woodlake", "Woodland", "Woodside", "Yorba Linda", 
+    "Yountville", "Yreka", "Yuba City", "Yucaipa", "Yucca Valley"
+]
+
+city_list = backup_cities
+
 def load_data_safe():
     try:
         df = pd.read_csv(GOOGLE_SHEET_URL)
@@ -21,74 +88,6 @@ def load_data_safe():
     except Exception:
         pass
 
-    # Danh sách toàn bộ 482 thành phố California chạy trực tiếp trong bộ nhớ
-    backup_cities = [
-        "Adelanto", "Agoura Hills", "Alameda", "Albany", "Alhambra", "Aliso Viejo", "Alturas", "Amador City", 
-        "American Canyon", "Anaheim", "Anderson", "Angels Camp", "Antioch", "Apple Valley", "Arcadia", "Arcata", 
-        "Arroyo Grande", "Artesia", "Arvin", "Atascadero", "Atherton", "Atwater", "Auburn", "Avalon", "Avenal", 
-        "Azusa", "Bakersfield", "Baldwin Park", "Banning", "Barstow", "Beaumont", "Bell", "Bell Gardens", 
-        "Bellflower", "Belmont", "Belvedere", "Benicia", "Berkeley", "Beverly Hills", "Big Bear Lake", "Biggs", 
-        "Bishop", "Blue Lake", "Blythe", "Bradbury", "Brawley", "Brea", "Brentwood", "Brisbane", "Buellton", 
-        "Buena Park", "Burbank", "Burlingame", "Calabasas", "Calexico", "California City", "Calimesa", "Calistoga", 
-        "Camarillo", "Campbell", "Canyon Lake", "Capitola", "Carlsbad", "Carmel-by-the-Sea", "Carpinteria", 
-        "Carson", "Cathedral City", "Ceres", "Cerritos", "Chico", "Chino", "Chino Hills", "Chula Vista", 
-        "Citrus Heights", "Claremont", "Clayton", "Clearlake", "Cloverdale", "Clovis", "Coachella", "Coalinga", 
-        "Colfax", "Colma", "Colton", "Colusa", "Commerce", "Compton", "Concord", "Corcoran", "Corning", 
-        "Corona", "Coronado", "Corte Madera", "Costa Mesa", "Cotati", "Covina", "Crescent City", "Cudahy", 
-        "Cupertino", "Cypress", "Daly City", "Dana Point", "Danville", "Davis", "Del Mar", "Del Rey Oaks", 
-        "Delano", "Desert Hot Springs", "Diamond Bar", "Dinuba", "Dixon", "Dorris", "Dos Palos", "Downey", 
-        "Duarte", "Dublin", "Dunsmuir", "East Palo Alto", "Eastvale", "El Cajon", "El Centro", "El Cerrito", 
-        "El Monte", "El Segundo", "Elk Grove", "Emeryville", "Encinitas", "Escalon", "Escondido", "Etna", 
-        "Eureka", "Exeter", "Fairfax", "Fairfield", "Farmersville", "Ferndale", "Fillmore", "Firebaugh", 
-        "Folsom", "Fontana", "Fort Bragg", "Fort Jones", "Fortuna", "Foster City", "Fountain Valley", "Fowler", 
-        "Fremont", "Fresno", "Fullerton", "Galt", "Garden Grove", "Gardena", "Gilroy", "Glendale", "Glendora", 
-        "Goleta", "Gonzalez", "Grand Terrace", "Grass Valley", "Greenfield", "Gridley", "Grover Beach", 
-        "Guadalupe", "Gustine", "Half Moon Bay", "Hanford", "Hawaiian Gardens", "Hawthorne", "Hayward", 
-        "Healdsburg", "Hemet", "Hercules", "Hermosa Beach", "Hesperia", "Hidden Hills", "Highland", "Hillsborough", 
-        "Hollister", "Holtville", "Hughson", "Huntington Beach", "Huntington Park", "Imperial", "Imperial Beach", 
-        "Indian Wells", "Indio", "Industry", "Inglewood", "Ione", "Irvine", "Irwindale", "Isleton", "Jackson", 
-        "Jurupa Valley", "Kerman", "King City", "Kingsburg", "La Cañada Flintridge", "La Habra", "La Habra Heights", 
-        "La Mesa", "La Mirada", "La Palma", "La Puente", "La Quinta", "La Verne", "Lafayette", "Laguna Beach", 
-        "Laguna Hills", "Laguna Niguel", "Laguna Woods", "Lake Elsinore", "Lake Forest", "Lakeport", "Lakewood", 
-        "Lancaster", "Larkspur", "Lathrop", "Lawndale", "Lemon Grove", "Lemoore", "Lincoln", "Lindsay", 
-        "Live Oak", "Livermore", "Livingston", "Lodi", "Loma Linda", "Lomita", "Lompoc", "Long Beach", 
-        "Loomis", "Los Alamitos", "Los Altos", "Los Altos Hills", "Los Angeles", "Los Banos", "Los Gatos", 
-        "Loyalton", "Lynwood", "Madera", "Malibu", "Mammoth Lakes", "Manhattan Beach", "Manteca", "Maricopa", 
-        "Marina", "Martinez", "Marysville", "Maywood", "McFarlin", "Mendota", "Menlo Park", "Merced", 
-        "Mill Valley", "Millbrae", "Milpitas", "Mission Viejo", "Modesto", "Monrovia", "Montague", "Montclair", 
-        "Monte Sereno", "Montebello", "Monterey", "Monterey Park", "Moorpark", "Moraga", "Moreno Valley", 
-        "Morgan Hill", "Morro Bay", "Mount Shasta", "Mountain View", "Murrieta", "Napa", "National City", 
-        "Needles", "Nevada City", "Newark", "Newman", "Newport Beach", "Norco", "Norwalk", "Novato", 
-        "Oakdale", "Oakland", "Oakley", "Oceanside", "Ojai", "Ontario", "Orange", "Orange Cove", "Orland", 
-        "Oroville", "Oxnard", "Pacific Grove", "Pacifica", "Palm Desert", "Palm Springs", "Palmdale", 
-        "Palo Alto", "Palos Verdes Estates", "Paradise", "Paramount", "Parlier", "Pasadena", "Paso Robles", 
-        "Patterson", "Perris", "Petaluma", "Pico Rivera", "Piedmont", "Pinole", "Pismo Beach", "Pittsburg", 
-        "Placentia", "Placerville", "Pleasant Hill", "Pleasanton", "Plymouth", "Point Arena", "Pomona", 
-        "Port Hueneme", "Porterville", "Portola", "Portola Valley", "Poway", "Rancho Cordova", "Rancho Cucamonga", 
-        "Rancho Mirage", "Red Bluff", "Redding", "Redlands", "Redondo Beach", "Redwood City", "Reedley", 
-        "Rialto", "Richmond", "Ridgecrest", "Rio Dell", "Rio Vista", "Ripon", "Riverbank", "Riverside", 
-        "Rocklin", "Rohnert Park", "Rolling Hills", "Rolling Hills Estates", "Rosemead", "Roseville", "Ross", 
-        "Sacramento", "Salinas", "San Anselmo", "San Bernardino", "San Bruno", "San Carlos", "San Clemente", 
-        "San Diego", "San Dimas", "San Fernando", "San Francisco", "San Gabriel", "San Jacinto", "San Joaquin", 
-        "San Jose", "San Juan Bautista", "San Juan Capistrano", "San Leandro", "San Luis Obispo", "San Marcos", 
-        "San Marino", "San Mateo", "San Pablo", "San Rafael", "San Ramon", "Sand City", "Sanger", "Santa Ana", 
-        "Santa Barbara", "Santa Clara", "Santa Clarita", "Santa Cruz", "Santa Fe Springs", "Santa Maria", 
-        "Santa Monica", "Santa Paula", "Santa Rosa", "Santee", "Saratoga", "Sausalito", "Scotts Valley", 
-        "Seal Beach", "Seaside", "Sebastopol", "Selma", "Shafter", "Shasta Lake", "Sierra Madre", "Signal Hill", 
-        "Simi Valley", "Solana Beach", "Soledad", "Solvang", "Sonoma", "Sonora", "South El Monte", "South Gate", 
-        "South Lake Tahoe", "South Pasadena", "South San Francisco", "St. Helena", "Stanton", "Stockton", 
-        "Suisun City", "Sunnyvale", "Susanville", "Sutter Creek", "Taft", "Tehachapi", "Tehama", "Temecula", 
-        "Temple City", "Thousand Oaks", "Tiburon", "Torrance", "Tracy", "Trinidad", "Truckee", "Tulare", 
-        "Tulelake", "Turlock", "Tustin", "Tustin", "Tustin", "Tustin", "Tustin", "Tustin", "Tustin", 
-        "Tustin", "Tustin", "Tustin", "Tustin", "Tustin", "Tustin", "Tustin", "Tustin", "Tustin", 
-        "Tustin", "Tustin", "Tustin", "Tustin", "Tustin", "Tustin", "Tustin", "Tustin", "Tustin", 
-        "Twentynine Palms", "Ukiah", "Union City", "Upland", "Vacaville", "Vallejo", "Ventura", "Vernon", 
-        "Victorville", "Villa Park", "Visalia", "Vista", "Walnut", "Walnut Creek", "Wasco", "Waterford", 
-        "Watsonville", "Weed", "West Covina", "West Hollywood", "West Sacramento", "Westlake Village", "Westminster", 
-        "Westmorland", "Wheatland", "Whittier", "Wildomar", "Williams", "Willits", "Willows", "Windsor", 
-        "Winters", "Woodlake", "Woodland", "Woodside", "Yorba Linda", "Yountville", "Yreka", "Yuba City", "Yucaipa", "Yucca Valley"
-    ]
-    
     backup_df = pd.DataFrame({
         'city': [c.lower() for c in backup_cities],
         'original_name': backup_cities,
@@ -100,11 +99,10 @@ def load_data_safe():
     return backup_df, "Hệ thống Dự phòng (Bộ nhớ Đám mây)"
 
 df_cities, data_source = load_data_safe()
-def get_city_from_address(address, df_city_list):
+def get_city_from_address(address, list_of_cities):
     cleaned_address = str(address).strip().lower()
-    for city in df_city_list:
-        city_str = str(city).strip().lower()
-        if city_str and city_str in cleaned_address:
+    for city in list_of_cities:
+        if str(city).strip().lower() in cleaned_address:
             return str(city).strip()
             
     try:
@@ -113,7 +111,7 @@ def get_city_from_address(address, df_city_list):
             optimized_address += ", CA"
         if "usa" not in address.lower():
             optimized_address += ", USA"
-        geolocator = Nominatim(user_agent="ca_civil_sow_generator_internal_production_v100")
+        geolocator = Nominatim(user_agent="ca_civil_sow_generator_internal_v105")
         location = geolocator.geocode(optimized_address, addressdetails=True, timeout=10)
         if location and 'address' in location.raw:
             address_details = location.raw['address']
@@ -124,7 +122,7 @@ def get_city_from_address(address, df_city_list):
             ]
             for place in possible_places:
                 if place:
-                    for city in df_city_list:
+                    for city in list_of_cities:
                         if str(city).strip().lower() == place.lower():
                             return str(city).strip()
     except Exception:
@@ -135,7 +133,6 @@ user_address = st.text_input("📍 Nhập địa chỉ dự án tại California
 if st.button("🔍 Tra cứu & Chuẩn bị SOW"):
     if user_address:
         with st.spinner("Hệ thống đám mây đang bóc tách địa chỉ dự án..."):
-            # 👉 ĐÃ SỬA: Khớp chính xác với biến 'city_list' được tạo ra từ kho dữ liệu độc lập ở Khối 1
             city_name = get_city_from_address(user_address, city_list)
             
             if city_name:
@@ -143,13 +140,19 @@ if st.button("🔍 Tra cứu & Chuẩn bị SOW"):
                 match = df_cities[df_cities[city_col_name].astype(str).str.strip().str.lower() == city_name.lower()]
                 
                 if not match.empty:
-                    city_info = match.iloc[0]
+                    city_info = match.iloc
                     display_city = city_info['original_name'] if 'original_name' in df_cities.columns else city_name
                     
-                    building_code_val = city_info['building_code']
-                    drainage_val = city_info['drainage civil specs']
-                    lid_val = city_info['low impact development']
-                    permit_agency_val = city_info['local permit agency']
+                    def get_column_value(keywords):
+                        for col in df_cities.columns:
+                            if any(kw in str(col).lower() for kw in keywords):
+                                return city_info[col]
+                        return "N/A"
+
+                    building_code_val = get_column_value(['building', 'structure', 'code', 'xây dựng'])
+                    drainage_val = get_column_value(['drainage', 'civil', 'spec', 'thoát nước'])
+                    lid_val = get_column_value(['low impact', 'lid', 'stormwater', 'nước mưa'])
+                    permit_agency_val = get_column_value(['permit', 'agency', 'local', 'cấp phép'])
                     
                     st.success(f"🎯 Đã xác định được thành phố: **{display_city}**")
                     st.caption(f"💾 Nguồn kết nối hiện tại: *{data_source}*")
